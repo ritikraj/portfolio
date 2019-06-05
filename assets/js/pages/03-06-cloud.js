bf15188d56ff385791c01daa34b337cb4be272ec
ritikraj <ritikraj559@gmail.com>
2019-06-05 16:20:03 +0530

    structure

--- assets/js/pages/03-06-cloud.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Eleven on 16-01-2019.
- */
-$(document).ready(function () {
-    $(".logo").html("<img class='' src='../../../assets/images/graphics/full-logo-white.svg'>");
-});
-
--- assets/js/pages/05-contact.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Eleven on 29-01-2019.
- */
-$(document).ready(function () {
-    $(".logo").html("<img class='' src='../../assets/images/graphics/full-logo-white.svg'>");
-});
